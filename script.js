4a641655d140f878f608cb7d5baca603a68a23e2
eightmouse <92474424+eightmouse@users.noreply.github.com>
2023-05-29 18:52:41 +0200

    Clearedthe code a little and made it case insensitive

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// playRound:
+//   player submits choice
+//   computer randomly chooses
+//   comparison happens
+//   score is updated
+//   round number is updated
+//   HTML is updated, displaying win/draw
+//   **Before the round ends, check if the game is over...
 
-
+//Game starts here!
 function game(Rock, Paper, Scissors) {
-
+    //This is the algorithm that randomly generates a choice for the CPU
     function getComputerChoice() {
         const choices = Math.random()
             if ( choices <= 0.1) {
@@ -13,30 +21,35 @@ function game(Rock, Paper, Scissors) {
             }
     }
 
+    //Rounds variables are stored here
     function playRound(playerSelection, computerSelection) {
         if (playerSelection === computerSelection) {
             return ("It's a draw, good one :)");
         } else if (( playerSelection === "Rock") && (computerSelection === "Scissors")) {
-            return ("You won, Rock beats Scissors!");
+            return ("You won, Rock beats Scissors :)");
         } else if (( playerSelection === "Rock") && (computerSelection === "Paper")) {
-            return ("You lost, nice try tho :P");
+            return ("You lost, Paper beats Rock :)");
         } else if (( playerSelection === "Paper") && (computerSelection === "Rock")) {
-            return ("You won, damn you're good!");
+            return ("You won, Paper beats Rock :)=");
         } else if (( playerSelection === "Paper") && (computerSelection === "Scissors")) {
-            return ("You lost, better luck next time?");
+            return ("You lost, Scissors beats Paper :)");
         } else if (( playerSelection === "Scissors") && (computerSelection === "Rock")) {
-            return ("You lost, hehe!");
+            return ("You lost, Rock beats Scissors :)!");
         } else if (( playerSelection === "Scissors") && (computerSelection === "Paper")) {
-            return ("You won, GG!");
+            return ("You won, Scissors beats paper :)");
         }
     }
 
+    //User prompt and computer choices are stored here
     let playerSelection = prompt("Choose wisely!");
-    // if (playerSelection === "Rock", "Paper", "Scissors") {
-    // } else (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
     let computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
+
 }
 
-// let start = game();
-console.log(game());
+//This loops the game 5 times
+for (let i = 0; i <= 5; i++) {
+    game();
+}
